3d39b869d86f167bff20e3ff70fe3ce3192a05da
Zongwei-Tang <tangzongwei070509@gmail.com>
2025-10-08 01:38:41 -0700

    Add a sidebar after learning JS

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById("sidebar")
+    const sidebutton = document.getElementById("sidebutton")
+    sidebutton.addEventListener("click", () => {
+        sidebar.classList.toggle('close')
+    })
+})
