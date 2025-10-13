96f3f98099f02ed517ca1e35966d510f63ee3ae9
Zongwei-Tang <tangzongwei070509@gmail.com>
2025-10-12 23:40:27 -0700

    Change the style to fulfill class's assignment requirement

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+    sidebar()
+    goToPhoto()
+})
+
+function sidebar(){
     const sidebar = document.getElementById("sidebar")
     const sidebutton = document.getElementById("sidebutton")
     sidebutton.addEventListener("click", () => {
         sidebar.classList.toggle('open')
+        if (sidebutton.innerText=="<"){
+            sidebutton.innerText=">"
+        }
+        else {
+            sidebutton.innerText="<"
+        }
     })
-})
+}
+
+function goToPhoto(){
+    const photobutton = document.getElementById("myphoto")
+    photobutton.addEventListener("click", () => {
+        window.location.href = "photos.html"
+    })
+}
